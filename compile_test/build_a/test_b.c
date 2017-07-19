eefbe4000dcdde4ed6c4fd450dbe7605798ce2d7
bruceshu <3350207067@qq.com>
2017-07-19 13:02:04 -0400

    整理代码

---
+++ compile_test/build_a/test_b.c
@@ -0,0 +1,8 @@
+#include "include/test.h"
+
+#include "stdio.h"
+
+void test_b()
+{
+	printf("test_b\n");
+}
---
+++ compile_test/build_so/test_a.c
@@ -0,0 +1,8 @@
+#include "include/test.h"
+
+#include "stdio.h"
+
+void test_a()
+{
+	printf("test a\n");
+}
