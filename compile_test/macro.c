3fcb3f547be49e7b2d7a3b97ccd9a80daf5ad42c
bruceshu <3350207067@qq.com>
2017-09-25 13:58:01 -0400

    整理代码

--- compile_test/macro.c
+++ compile_test/macro.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define __cplusplus 1
 
 int main(int argc, char *argv[])
 {
-	printf("%s ", __FILE__);
-	printf("%s ", __func__);
-	rintf("%d\n", __LINE__);
+	printf("%s:%s:%d\n ", __FILE__, __func__, __LINE__);
 	printf("__GNUC__=%d\n", __GNUC__);
 	printf("__cplusplus=%d\n", __cplusplus);
+//	printf("%s", __VA_ARGS__);	
+
+
+	printf("getenv FFREPROT addr=%p\n", getenv("FFREPORT"));
+	printf("getenv FFREPORT=%s\n", getenv("FFREPORT"));		
+	printf("getenv PATH addr=%p\n", getenv("PATH"));
+	printf("getenv=%s\n", getenv("PATH"));
+	
 	return 0;
 }
