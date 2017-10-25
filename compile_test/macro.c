729ba0170ca1954f4ddecee5a0414f4bc36a7189
bruceshu <3350207067@qq.com>
2017-10-25 15:36:29 +0800

    增加宏用例

--- compile_test/macro.c
+++ compile_test/macro.c
@@ -2,18 +2,24 @@
 #include <stdlib.h>
 #define __cplusplus 1
 
+#define CONNECT(a, b) a##b
+#define STR(a) #a
+#define TEST(a) STR(a)
+
 int main(int argc, char *argv[])
 {
 	printf("%s:%s:%d\n ", __FILE__, __func__, __LINE__);
 	printf("__GNUC__=%d\n", __GNUC__);
-	printf("__cplusplus=%d\n", __cplusplus);
-//	printf("%s", __VA_ARGS__);	
-
 
 	printf("getenv FFREPROT addr=%p\n", getenv("FFREPORT"));
 	printf("getenv FFREPORT=%s\n", getenv("FFREPORT"));		
 	printf("getenv PATH addr=%p\n", getenv("PATH"));
 	printf("getenv=%s\n", getenv("PATH"));
+
+	printf("c:%d, %x, %X\n", 'c', 'c', 'c');
+	
+	printf("STR(CONNECT(con, 2))=%s\n", STR(CONNECT(con, 2)));
+	printf("TEST(CONNECT(con, 2))=%s\n", TEST(CONNECT(con, 2)));
 	
 	return 0;
 }
