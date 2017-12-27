7071ae182175b4a58f3fd73a5c09c7ae046e1e12
bruceshu <bruceshu@github.com>
2017-12-27 17:36:01 +0800

    增加系统函数strspn和strcspn测试用例

--- compile_test/sys_func_test.c
+++ compile_test/sys_func_test.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
-//#include <atomic.h>
+#include <string.h>
+
+void test1();
+void test2();
 
 int main(int argc, char * argv[])
+{
+//	test1();
+
+	test2();
+
+	return 0;
+}
+
+/* 测试原子函数 */
+void test1()
 {
 	int operand = 10;
 	int result = 0;
 
 	result = __sync_fetch_and_add(&operand, 1);
-
 	printf("result = %d\n", result);
 	printf("operand = %d\n", operand);
+}
 
-	return 0;
+void test2()
+{
+	char *str1 = "abcdefghijklmnopqrstuvwxyz";
+	char *str2 = "shuhuan";
+	char *str3 = "123456";
+
+	int result = 0;
+
+	result = strspn(str1, str3);
+	printf("the result of strspn is %d\n", result);
+
+	result = strcspn(str1, str3);
+	printf("the result of strcspn is %d\n", result);
 }
