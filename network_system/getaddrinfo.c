ffb6566f6c474d8e836fb22b168dbc7580712a55
bruceshu <3350207067@qq.com>
2018-07-18 15:16:53 +0800

    修改getaddrinfo测试代码

--- network_system/getaddrinfo.c
+++ network_system/getaddrinfo.c
@@ -5,6 +5,12 @@
 *
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+
 int main(int argc, char *argv[])
 {
     struct addrinfo hints;
@@ -12,10 +18,13 @@ int main(int argc, char *argv[])
     struct addrinfo *cur_ai;
     struct sockaddr_in *addr;
     
-    char ipaddr[16] = {0};
-    char *hostname = "35.197.157.137";
+    int ret;
+    char *hostname = "www.bai.com";
     char portstr[8] = {0};
     int port = 80;
+
+    char ipaddr[16] = {0};
+    char *ip_ptr = NULL;
 
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
@@ -28,14 +37,21 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    for (cur_ai = res; cur_ai != NULL; cur_ai = cur_ai->next) {
+    for (cur_ai = res; cur_ai != NULL; cur_ai = cur_ai->ai_next) {
         addr = (struct sockaddr_in *)cur_ai->ai_addr;
-        sprintf(ipaddr, "%d.%d.%d.%d", addr->sin_addr.S_un.s_un_b.s_b1, 
-                                       addr->sin_addr.S_un.s_un_b.s_b2,
-                                       addr->sin_addr.S_un.s_un_b.s_b3,
-                                       addr->sin_addr.S_un.s_un_b.s_b4);
 
-        printf("ipaddr = %s\n", ipaddr);
+	//
+	//
+	//printf("sizeof(addr->sin_addr) = %d\n", sizeof(addr->sin_addr));
+        /*sprintf(ipaddr, "%d.%d.%d.%d", (*addr).sin_addr.s_un.S_un_b.s_b1, 
+                                       addr->sin_addr.S_un.S_un_b.s_b2,
+                                       addr->sin_addr.S_un.S_un_b.s_b3,
+                                       addr->sin_addr.S_un.S_un_b.s_b4);
+
+*/      
+	ip_ptr = inet_ntoa(addr->sin_addr);
+	printf("ip_ptr = %s\n", ip_ptr);  
+	//printf("ipaddr = %s\n", ipaddr);
     }
 
     freeaddrinfo(res);
