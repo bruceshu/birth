7c8249ad32ecf5059646416e7679cc00c0c4840c
bruceshu <3350207067@qq.com>
2018-10-10 11:15:09 +0800

    补充头文件

--- network_system/server-client/udp_server.c
+++ network_system/server-client/udp_server.c
@@ -1,4 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
 
 #define SERVER_PORT 8888
 #define BUFF_LEN 1024
