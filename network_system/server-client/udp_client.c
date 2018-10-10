688f4ae42d2ee5b9a600b2a2f3be3405d392ff6f
bruceshu <3350207067@qq.com>
2018-10-10 11:13:16 +0800

    增加udp网络编程代码

---
+++ network_system/server-client/udp_client.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+#define SERVER_PORT 8888
+#define BUFF_LEN 1024
+#define SERVER_IP "192.168.140.112"
+
+void udp_msg_sender(int fd, struct sockaddr *dst)
+{
+    socklen_t len;
+    struct sockaddr_in src;
+
+    while(1) {
+        char buf[BUFF_LEN] = "test udp msg\n";
+        len = sizeof(*dst);
+
+        sendto(fd, buf, BUFF_LEN, 0, dst, len);
+        memset(buf, 0, BUFF_LEN);
+
+        sleep(1);
+    }
+}
+
+int main(int argc, char * argv[])
+{
+    int client_fd;
+    struct sockaddr_in ser_addr;
+
+    client_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (client_fd < 0) {
+        printf("create socket failed!\n");
+        return -1;
+    }
+
+    memset(&ser_addr, 0, sizeof(ser_addr));
+    ser_addr.sin_family = AF_INET;
+    //ser_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    ser_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    ser_addr.sin_port = htons(SERVER_PORT);
+
+    udp_msg_sender(client_fd, (struct sockaddr*)&ser_addr);
+
+    close(client_fd);
+    return 0;
+}
---
+++ network_system/server-client/udp_server.c
@@ -0,0 +1,52 @@
+
+
+#define SERVER_PORT 8888
+#define BUFF_LEN 1024
+
+void handle_udp_msg(int fd)
+{
+    char buf[BUFF_LEN] = {0};
+    socklen_t len;
+    int count;
+    struct sockaddr_in client_addr;
+
+    while(1) {
+        len = sizeof(client_addr);
+        count = recvfrom(fd, buf, BUFF_LEN, 0, (struct sockaddr*)&client_addr, &len);
+        if (count == -1) {
+            printf("recieve data failed!\n");
+            return;
+        }
+
+        printf("client:%s\n", buf);
+    }
+}
+
+int main(int argc, char * argv[])
+{
+    int server_fd;
+    int ret;
+    struct sockaddr_in ser_addr;
+
+    server_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (server_fd < 0) {
+        printf("create socket failed!\n");
+        return -1;
+    }
+
+    memset(&ser_addr, 0, sizeof(ser_addr));
+    ser_addr.sin_family = AF_INET;
+    ser_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    ser_addr.sin_port = htons(SERVER_PORT);
+
+    ret = bind(server_fd, (struct sockaddr*)&ser_addr, sizeof(ser_addr));
+    if (ret < 0) {
+        printf("socket bind fail!\n");
+        return -1;
+    }
+
+    handle_udp_msg(server_fd);
+
+    close(server_fd);
+    return 0;
+}
