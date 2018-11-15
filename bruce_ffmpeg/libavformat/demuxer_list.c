e594b484fc318b3909e81320694b0d7a06c47a28
bruceshu <3350207067@qq.com>
2018-11-15 18:30:11 +0800

    整理编译脚本

--- bruce_ffmpeg/libavformat/demuxer_list.c
+++ bruce_ffmpeg/libavformat/demuxer_list.c
@@ -1,7 +1,7 @@
 static const AVInputFormat * const demuxer_list[] = {
-	//&ff_aac_demuxer,
-	//&ff_ac3_demuxer,
-	//&ff_flv_demuxer,
+//&ff_aac_demuxer,
+//&ff_ac3_demuxer,
+//&ff_flv_demuxer,
     //&ff_hls_demuxer,
     &ff_mpegts_demuxer,
     //&ff_rtp_demuxer,
