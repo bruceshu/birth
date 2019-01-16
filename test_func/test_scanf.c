0d2131160a77062d8c72d50c1bb6b876d796fbb5
bruceshu <3350207067@qq.com>
2019-01-15 19:14:44 -0800

    perfect scanf func

--- test_func/test_scanf.c
+++ test_func/test_scanf.c
@@ -5,15 +5,18 @@ int main()
     int a;
     char b;
     char c;
+    char d;
 
     printf("pls input a number\n");
     scanf("%d", &a);
-    scanf("%c", &b);
+    scanf("%c", &b); //jie shou \n
     scanf("%c", &c);
+    scanf("%c", &d);
 
     // 测试从控制台敲“enter”，在scanf中如何处理
     printf("a = %d\n", a);
-    printf("b = %c\n", b);    
-    printf("b = %c\n", c);
+    printf("b = 0X%02x\n", b);    
+    printf("c = 0X%02x\n", c);
+    printf("d = 0X%02x\n", d);
     return 0;
 }
